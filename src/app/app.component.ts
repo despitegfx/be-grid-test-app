b37782a80c1e8a8a7b8285b1769d7592eddc86ef
ebonsu <emmanuel.bonsu@solv.com.gh>
2024-10-03 09:03:27 +0000

    updated be-grid v1.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import DefColumns from './components/model/DefColumns';
+import {ContextMenu} from "./components/model/context-menu";
+import {ContextMenuService} from "be-grid";
+// import {ContextMenuService} from "./components/context-menu/context-menu.service";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +13,7 @@ export class AppComponent implements OnInit{
   title = 'be-grid';
 
   ELEMENT_DATA: any[] = [
-    {fred: 2, name: 'Helium', weight: 4.0026, symbol: 'He', under: "bug", age: 12, gender: 'Male', },
+    {fred: 2, name: 'Helium', weight: 4.0026, symbol: 'He', under: "bug" },
     {fred: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', under: "Testing"},
     {fred: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', under: "Testing"},
     {fred: 5, name: 'Boron', weight: 10.811, symbol: 'B', under: "C"},
@@ -18,11 +21,11 @@ export class AppComponent implements OnInit{
     {fred: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', under: "Testing"},
     {fred: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', under: "Testing"},
     {fred: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', under: "Testing"},
-    {fred: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', under: "Testing", others: "hejrhe"},
+    {fred: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', under: "Testing"},
   ];
-  
+
   defColumns: DefColumns[] = [
-    {name: "fred", label: "Fred", columnSortable: false, rowSelection: true}, 
+    {name: "fred", label: "Fred", columnSortable: false, rowSelection: true},
     {name: "name", columnSortable: false, cellStyle: (data: any) => {return (data.name == "Boron")? {background:'green', color: 'white'}: ""} },
     {name: "weight", label: "Weight", cellFlag: (data: any) => {return (data.weight == 4.0026)? {style:{ background:'yellow', color: 'black'}, flagText: "primary"}: ""} },
     {name: "symbol", columnSortable: true},
@@ -39,15 +42,42 @@ export class AppComponent implements OnInit{
   contextmenuX = 0;
   contextmenuY = 0;
 
-  ngOnInit(): void {}
+  selectedRows: any[] = []
+  contextMenu: ContextMenu[] = []
+
+  constructor(private api: ContextMenuService) {
+  }
+
+  ngOnInit(): void {
+    this.updateContext()
+  }
+
+  updateContext(){
+    this.contextMenu = []
+
+    this.contextMenu.push({
+      label: "test",
+      icon: { className: "fa-solid fa-shield", color: "red" },
+      action: ()=>{}
+    })
+
+    if (this.selectedRows.length > 0){
+      this.contextMenu.push({
+        label: "okay",
+        icon: { className: "fa-solid fa-message", color: "blue" },
+        action: ()=>{}
+      })
+    }
+    this.api.contextMenu(this.contextMenu)
+  }
 
   onSelectedRow(event:any){
     this.selectedRow = event;
   }
 
   rowSelection(event:any){
-    console.log(event);
-    
+    this.selectedRows = event.selectedRows
+    this.updateContext()
   }
 
 }
